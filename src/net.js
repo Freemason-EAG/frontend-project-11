705d25483b263074f2f60f479c8eb71b84ecf138
Freemason-EAG <egoyan.arutyun@mail.ru>
2026-01-12 12:36:15 +0300

    replace axios with fetch for tests

--- src/net.js
+++ src/net.js
@@ -1,12 +1,16 @@
-import axios from 'axios'
-
 const netRequest = (url) => {
   const allOriginsUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`
 
-  return axios.get(allOriginsUrl)
+  return fetch(allOriginsUrl)
     .then((response) => {
-      if (response.data.status.http_code === 200) {
-        return response.data.contents
+      if (!response.ok) {
+        throw new Error(`${response.status}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      if (data.status.http_code === 200) {
+        return data.contents
       }
       throw new Error('Network response was not ok.')
     })
@@ -16,3 +20,22 @@ const netRequest = (url) => {
 }
 
 export default netRequest
+
+// import axios from 'axios'
+
+// const netRequest = (url) => {
+//   const allOriginsUrl = `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`
+
+//   return axios.get(allOriginsUrl)
+//     .then((response) => {
+//       if (response.data.status.http_code === 200) {
+//         return response.data.contents
+//       }
+//       throw new Error('Network response was not ok.')
+//     })
+//     .catch ((error) => {
+//       throw new Error(`Network error: ${error.message}`)
+//     })
+// }
+
+// export default netRequest
